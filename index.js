e345922491812e2071ead146d688c228100445a7
Natalia Gruzdeva <Natalia_Gruzdeva@epam.com>
2017-04-02 15:03:24 +0300

    bandUseTable

--- index.js
+++ index.js
@@ -13,14 +13,18 @@ function Gnd(options) {
     var h = options.h;
     var r_permissible = options.r_permissible;
 
+    var alRatio = Math.round(this.a/this.l);
+    alRatio = Math.max(1, alRatio);
+    alRatio = Math.min(3, alRatio);
+
+    this.alRatio = alRatio;
     this.r_calc = soilResistivity * seasonalityCoefficient;
     var t = h + 0.5 * l;
     this.r_pipe = 0.336 * this.r_calc * (Math.log10(2 * l / d) + 0.5 * Math.log10((4 * t + l) / (4 * t - l))) / l;
-    this.r_pipe = 58.43;
     this.n_approximate = Math.ceil(this.r_pipe / r_permissible);
 
     if (this.isRow) {
-        this.table = [
+        this.pipeUseTable = [
             {
                 count: 4,
                 coefficients: [0.77, 0.89, 0.92]
@@ -54,10 +58,36 @@ function Gnd(options) {
                 coefficients: [0.2, 0.34, 0.47]
             }
         ];
+        this.bandUseTable = [
+            {
+                count: 5,
+                coefficients: [0.72, 0.85, 0.9]
+            },
+            {
+                count: 10,
+                coefficients: [0.59, 0.7, 0.79]
+            },
+            {
+                count: 20,
+                coefficients: [0.4, 0.55, 0.65]
+            },
+            {
+                count: 30,
+                coefficients: [0.3, 0.45, 0.57]
+            },
+            {
+                count: 50,
+                coefficients: [0.21, 0.35, 0.49]
+            },
+            {
+                count: 70,
+                coefficients: [0.19, 0.33, 0.46]
+            }
+        ];
         return;
     }
 
-    this.table = [
+    this.pipeUseTable = [
         {
             count: 4,
             coefficients: [0.45, 0.55, 0.7]
@@ -94,7 +124,38 @@ function Gnd(options) {
             count: 100,
             coefficients: [0.19, 0.24, 0.33]
         }
-    ]
+    ];
+
+    this.bandUseTable = [
+        {
+            count: 5,
+            coefficients: [0.41, 0.5, 0.71]
+        },
+        {
+            count: 10,
+            coefficients: [0.33, 0.39, 0.55]
+        },
+        {
+            count: 20,
+            coefficients: [0.27, 0.32, 0.44]
+        },
+        {
+            count: 30,
+            coefficients: [0.23, 0.3, 0.4]
+        },
+        {
+            count: 50,
+            coefficients: [0.21, 0.27, 0.37]
+        },
+        {
+            count: 70,
+            coefficients: [0.2, 0.25, 0.35]
+        },
+        {
+            count: 100,
+            coefficients: [0,19, 0.24, 0.33]
+        }
+    ];
 }
 
 Gnd.prototype.findN = function (pipeUse) {
@@ -117,11 +178,8 @@ Gnd.prototype.findR_common = function (n, bandUse) {
     this.r_common = this.r_pipe * this.r_band / (n * this.r_band * this.pipeUse + this.r_pipe * bandUse);
 };
 
-Gnd.prototype.getPipeUse = function () {
-    var coef = Math.round(this.a/this.l);
-    coef = Math.max(1, coef);
-    coef = Math.min(3, coef);
-    var table = this.table;
+Gnd.prototype.getFromTable = function (table) {
+    var alRatio = this.alRatio;
     var diff = Infinity;
     var diffI = -1;
 
@@ -138,11 +196,15 @@ Gnd.prototype.getPipeUse = function () {
         diffI = table.length - 1;
     }
 
-    return table[diffI].coefficients[coef];
+    return table[diffI].coefficients[alRatio];
+};
+
+Gnd.prototype.getPipeUse = function () {
+    return this.getFromTable(this.pipeUseTable);
 };
 
 Gnd.prototype.getBandUse = function () {
-    return 0.65;
+    return this.getFromTable(this.bandUseTable);
 };
 
 var gnd, pipeUse, bandUse, n;
